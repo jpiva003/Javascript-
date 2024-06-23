e2c10ee90ac1d0bf2ed3b114e331662d03ee2a84
Jenfer Pivaral <jpiva003@ucr.edu>
2024-06-22 18:00:40 -0700

    created random number and then function to return a string from that number

--- java.js
+++ java.js
@@ -1,24 +1,25 @@
 // function to output results of one round
 function playRound() {
-  if (computerChoice === playerChoice) {
+  if (computerChoice === playerAnswer) {
     return "Tie";
-  } else if (playerChoice === "rock" && computerChoice === "scissors") {
+  } else if (playerAnswer === "rock" && computerChoice === "scissors") {
     return "Player wins!";
-  } else if (playerChoice === "paper" && computerChoice === "rock") {
+  } else if (playerAnswer === "paper" && computerChoice === "rock") {
     return "Player wins!";
-  } else if (playerChoice === "scissors" && computerChoice === "paper") {
+  } else if (playerAnswer === "scissors" && computerChoice === "paper") {
     return "Player wins!";
   } else {
     return "Player loses!";
   }
 }
-// function to generate random number from 0-2
-function randomNum() {
+// function to generate random number
+function generateComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
-// function to convert random number into an output in string
-let number = randomNum();
-function computerChoice() {
+let number = generateComputerChoice();
+console.log(number);
+// function to return string using random number for computerChoice
+function returnString() {
   if (number === 0) {
     return "rock";
   } else if (number === 1) {
@@ -27,3 +28,4 @@ function computerChoice() {
     return "scissors";
   }
 }
+console.log(returnString());
